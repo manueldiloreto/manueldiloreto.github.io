e74b55df2eea1d24a5120b8ed5fd645bb3474594
Manuel Di Loreto <120795430+manueldiloreto@users.noreply.github.com>
2022-12-27 16:57:21 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@
 filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("column");
+  x = document.getElementsByClassName("card");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
